4c90c5fef7d6fb0d693f1aeff360bcda9e2237a3
ypoffet <yannick.poffet@bluebotics.com>
2021-03-05 17:54:50 +0100

    Added dummy Storecontroller when testing web

--- StoreBackEnd/app.py
+++ StoreBackEnd/app.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from StoreController import StoreController
+from DummyController import DummyController
+
+dummyMode = True
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
-storeController = StoreController()
+
+if dummyMode == True:
+    storeController = DummyController()
+else:
+    from StoreController import StoreController
+    storeController = StoreController()
 
 
 @app.route('/json_post', methods = ['POST'])
@@ -31,6 +38,9 @@ def storeDisplay():
     storeList = storeController.clusterOfStores.listOfStores
     return render_template('index.html', tasks=storeList)
 
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
 
 if __name__ == "__main__":
     app.run(host= '0.0.0.0')
