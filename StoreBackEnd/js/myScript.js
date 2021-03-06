3fa9f536a7e88a244284e4f5ef92092eaae257be
yannickstorm <yannick@poffet.net>
2021-03-06 01:51:55 +0100

    Created javascript function to send api command to store

--- StoreBackEnd/js/myScript.js
+++ StoreBackEnd/js/myScript.js
@@ -1,12 +1,15 @@
 
 
 
-function myFunction() {
+function sendCommandToStore(store, cmd) {
   document.getElementById("demo").innerHTML = "Paragraph changed.";
 
-  fetch('localhost:5000/store/')
-  .then(response => response.json())
-  .then(data => console.log(data));
+  //fetch('http://localhost:5000/store/1/0')
+  //.then(response => response.json())
+  //.then(data => console.log(data));
+
+  var pathToBackEnd = "http://localhost:5000/store/"
+  fetch(pathToBackEnd + String(store) + "/" + String(cmd));
 }
 
 
